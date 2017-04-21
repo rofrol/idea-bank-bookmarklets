f1ccafe97d9b5803414b35521f4d86c04ef00a75
Roman Frołow <rofrol@gmail.com>
2017-04-21 16:12:31 +0200

    async for waiting - should make in a loop checking

--- ppl.js
+++ ppl.js
@@ -1,20 +1,8 @@
 //javascript:
 (function () {
   console.log('PPL');
-  document.querySelector('[href="javascript:getUs();"]').click();
-  /* Piaseczno */
-  document.querySelector('#us_1_msa_191').click();
-  /* Urząd skarbowy */
-  document.querySelector('#us_2_msa_1').click();
-  /* PPL */
-  document.querySelector('#us_3_msa_111').click();
-  /* 37101010100161862223000000 */
-  document.querySelector('#us_4_msa_1').click();
-  /* Zapisz */
-  document.getElementById('37101010100161862223000000').click();
-  /* M - Miesiąc */
-  document.querySelector('#interval_msa_4').click();
-  /* Poprzedni miesiąc interv_msa_3 */
+
+  /* Poprzedni miesiąc */
   function previousMonthAndYear() {
     var dateObj = new Date();
     dateObj.setMonth(dateObj.getMonth() - 1);
@@ -22,10 +10,37 @@
     var year = dateObj.getUTCFullYear();
     return { month, year };
   };
-  var previousDate = previousMonthAndYear();
-  document.querySelector('#interv_msa_' + previousDate.month).click();
 
-  /* Year */
-  Array.prototype.slice.call(document.querySelectorAll('[id^="year_msa_"] '))
-    .find(e => e.querySelector('span').textContent == previousDate.year).click()
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function run(milisec) {
+    /* Pobierz numer */
+    document.querySelector('[href="javascript:getUs();"]').click();
+    await sleep(1000);
+    /* Piaseczno */
+    document.querySelector('#us_1_msa_191').click();
+    await sleep(500);
+    /* Urząd skarbowy */
+    document.querySelector('#us_2_msa_1').click();
+    await sleep(500);
+    /* PPL */
+    document.querySelector('#us_3_msa_111').click();
+    await sleep(500);
+    /* 37101010100161862223000000 */
+    document.querySelector('#us_4_msa_1').click();
+    /* Zapisz */
+    document.getElementById('37101010100161862223000000').click();
+    /* M - Miesiąc */
+    document.querySelector('#interval_msa_4').click();
+
+    var previousDate = previousMonthAndYear();
+    document.querySelector('#interv_msa_' + previousDate.month).click();
+
+    /* Year */
+    Array.prototype.slice.call(document.querySelectorAll('[id^="year_msa_"] '))
+      .find(e => e.querySelector('span').textContent == previousDate.year).click()
+  }
+  run();
 })();
