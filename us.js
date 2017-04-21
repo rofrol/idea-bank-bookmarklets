be4fdd87357899d425ed849270408315d518259e
Roman Frołow <rofrol@gmail.com>
2017-04-21 16:34:28 +0200

    Update and rename ppl.js to us.js

--- us.js
+++ us.js
@@ -15,37 +15,42 @@
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  async function run(milisec) {
+  async function run(formularz_type) {
     /* Przelew do US */
     document.querySelector('[onclick="showform(\'transferUs\')"]').click();
-    await sleep(500);
+    await sleep(1000);
     /* Pobierz numer */
     document.querySelector('[href="javascript:getUs();"]').click();
-    await sleep(500);
+    await sleep(1000);
     /* Piaseczno */
     document.querySelector('#us_1_msa_191').click();
-    await sleep(500);
+    await sleep(1000);
     /* Urząd skarbowy */
     document.querySelector('#us_2_msa_1').click();
-    await sleep(500);
-    /* PPL */
-    document.querySelector('#us_3_msa_111').click();
-    await sleep(500);
-    /* 37101010100161862223000000 */
+    await sleep(1000);
+    /* formularz_type */
+    document.querySelector(formularz_type.id).click();
+    await sleep(1000);
+    /* select bank account */
     document.querySelector('#us_4_msa_1').click();
-    await sleep(500);
+    await sleep(1000);
     /* Zapisz */
-    document.getElementById('37101010100161862223000000').click();
-    await sleep(500);
+    document.getElementById(formularz_type.bank).click();
+    await sleep(1000);
     /* M - Miesiąc */
     document.querySelector('#interval_msa_4').click();
-    await sleep(500);
+    await sleep(1000);
     var previousDate = previousMonthAndYear();
     document.querySelector('#interv_msa_' + previousDate.month).click();
-    await sleep(500);
+    await sleep(1000);
     /* Year */
     Array.prototype.slice.call(document.querySelectorAll('[id^="year_msa_"] '))
       .find(e => e.querySelector('span').textContent == previousDate.year).click()
   }
-  run();
+  var formularz = {
+    ppl: { id: "#us_3_msa_111", bank: "37101010100161862223000000" },
+    vat7: { id: "#us_3_msa_138", bank: "87101010100161862222000000" }
+  };
+  /* run(formularz.ppl); */
+  /* run(formularz.vat7); */
 })();
