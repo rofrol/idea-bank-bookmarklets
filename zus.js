b63f1c47c22b5ac844e250b43ad55c4700f5174e
Roman Frołow <rofrol@gmail.com>
2017-04-21 16:44:22 +0200

    zus.js - correct previous month

--- zus.js
+++ zus.js
@@ -1,21 +1,29 @@
 //javascript:
 (function () {
+  /* Poprzedni miesiąc */
+  function previousMonthAndYear() {
+    var dateObj = new Date();
+    dateObj.setMonth(dateObj.getMonth() - 1);
+    var month = dateObj.getUTCMonth() + 1;
+    var year = dateObj.getUTCFullYear();
+    return { month, year };
+  };
+
   function set(id, amount) {
     document.querySelector(id).click();
     document.querySelector('#payment_typ_msa_5').click();
     document.querySelector('[name="declaration_number"]').value = '01';
 
     function zusDeclarationDate() {
-      var dateObj = new Date();
-      var month = dateObj.getUTCMonth();
-      month = month < 10 ? '0' + month : month;
-      var year = dateObj.getUTCFullYear();
-      return '' + month + year;
+      var d = previousMonthAndYear();
+      var month = d.month < 10 ? '0' + d.month : '' + d.month;
+      return '' + month + d.year;
     };
+
     document.querySelector('[name="declaration_date"]').value = zusDeclarationDate();
     document.querySelector('tr:nth-child(12) [name="amount"]').value = amount;
   }
-  function spoleczne(){ set('#rb_msa_1', '749.94'); };
-  function zdrowotne(){ set('#rb_msa_2', '297.28'); };
-  function fundusz(){ set('#rb_msa_3', '62.67'); };
+  function spoleczne() { set('#rb_msa_1', '749.94'); };
+  function zdrowotne() { set('#rb_msa_2', '297.28'); };
+  function fundusz() { set('#rb_msa_3', '62.67'); };
 })();
